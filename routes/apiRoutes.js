bea7bc68bedec52005548f9d57344b2f44d8bfc3
WLeondike <billyleondike@gmail.com>
2020-07-12 23:27:30 -0400

    notes save, working on delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,26 @@
-const { v1: uuidv1 } = require('uuid');
+const fs = require("fs");
+const { v1: uuidv1 } = require("uuid");
 
+module.exports = (app) => {
+  app.get("/api/notes", (req, res) => {
+    const noteData = JSON.parse(fs.readFileSync(`${__dirname}/../db/db.json`, "utf8"));
+    res.json(noteData);
+  });
 
+  app.post("/api/notes", (req, res) => {
+    const data = fs.readFileSync(`${__dirname}/../db/db.json`, "utf8");
+    let notesArr = JSON.parse(data);
+    const usrNote = req.body;
+    usrNote.id = uuidv1();
+    notesArr.push(usrNote);
+    fs.writeFileSync(`${__dirname}/../db/db.json`, JSON.stringify(notesArr));
+    res.send("Success!");
+  });
 
-app.post("/api/notes", (req,res) => {
-    let noteID = uuidv1();
-    let usrNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: noteID
-    }
-    
-
-})
-
-app.delete("/api/notes/:id", (req,res) => {
-
-
-})
+  app.delete("/api/notes/:id", (req, res) => {
+    let usrNote = req.params.id;
+    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+      res.json(db);
+    });
+  });
+};
